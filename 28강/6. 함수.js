51cdf2350ca79615a020c6c69a29b4001835c5de
woogi25 <hyunwook1891@gmail.com>
2022-11-13 19:16:39 +0900

    JS 함수 공부

---
+++ 28강/6. 함수.js
@@ -0,0 +1,69 @@
+// // 1 ~ 10 더하기
+
+// let sum = 0;
+
+
+// for (let i = 1; i <= 10; i++) {
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+// // 1 ~ 20 더하기
+
+// for(let i = 1; i <= 20; i++) {
+//     sum = sum + i
+// }
+
+// console.log(sum);
+
+//기명함수
+// function sum(start,target) {
+//     let result = 0;
+//     for (let i = start; i <= target; i++) {
+//         result= result + i;
+//     }
+
+//     return result;
+// }
+
+//익명 함수
+// const sum = function (start,target) {
+//     let result = 0;
+//     for (let i = start; i <= target; i++) {
+//         result= result + i;
+//     }
+
+//     return result;
+// }
+
+//익명함수
+const sum = (start,target) => {
+    let result = 0;
+    for (let i = start; i <= target; i++) {
+        result= result + i;
+    }
+
+    return result;
+}
+
+console.log(sum(10, 30));
+// console.log(sum(50));
+// console.log(sum(100));
+
+
+
+// 호이스팅
+//기명함수 = 호이스팅 지원
+//익명함수 = 호이스팅 미지원
+
+// bmi 함수 만들기
+
+function BMI(height, weight) {
+    const meterHeight = height / 100;
+    const result = weight / (meterHeight * meterHeight);
+
+    return result;
+}
+
+console.log(BMI(170, 70));
