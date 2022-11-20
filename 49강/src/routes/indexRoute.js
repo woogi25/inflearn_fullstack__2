0a724128ce1952a78d3a72c38d1febb5dc02907e
woogi25 <hyunwook1891@gmail.com>
2022-11-20 21:46:36 +0900

    49강 수정 API 만드는 방법 공부

---
+++ 49강/src/routes/indexRoute.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+  const index = require("../controllers/indexController");
+  const jwtMiddleware = require("../../config/jwtMiddleware");
+
+  // 라우터 정의
+  // app.HTTP메서드(uri, 컨트롤러 콜백함수)
+  // app.get("/dummy", index.example);
+
+  // 학생 테이블 조회
+  app.get("/students/:studentIdx", index.readStudents);
+  // 학생 생성
+  app.post("/students", index.createStudent);
+  // 학생 업데이트
+  app.patch("/students/:studentIdx", index.updateStudents);
+  };
+
